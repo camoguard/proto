12678118532b4096f6a087459964494834a2a8be
sdornan <sam@whiskeymedia.com>
2012-02-03 15:55:27 -0800

    Forgot some stuff

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ SETUP
 2.  `pip install -r requirements/development.txt`
 3.  Create an empty `site_media` folder in the project base directory
 4.  `python manage.py syncdb` and follow prompts
-5.  `python manage.py runserver`
+5.  `python manage.py collectstatic`
+6.  `python manage.py runserver`
--- proto/urls.py
+++ proto/urls.py
@@ -11,21 +11,15 @@ urlpatterns = patterns('',
     # url(r'^proto/', include('proto.foo.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^accounts/', include('registration.urls')),
-
     url(r'^accounts/profile/', 'proto.accounts.views.profile', name='profile'),
 
-    url(r'^messages/', include('django_messages.urls')),
-
     url(r'^grappelli/', include('grappelli.urls')),
-
     url(r'^admin/filebrowser/', include(site.urls)),
 
     url(r'^news/', include('proto.news.urls')),
-
     url(r'^forums/', include('proto.forums.urls')),
 )
 
