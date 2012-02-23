18f659392d42dd4ec58da920c708417ccfbce5b4
sdornan <sam@whiskeymedia.com>
2012-02-23 14:58:25 -0800

    Added creole markup filter

---
+++ proto/wiki/templatetags/markup.py
@@ -0,0 +1,18 @@
+from django import template
+from django.conf import settings
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+def creole(value):
+    try:
+        import creole
+    except ImportError:
+        if settings.DEBUG:
+            raise template.TemplateSyntaxError("Error in {% creole %} filter: The Python creole library isn't installed.")
+        return force_unicode(value)
+    else:
+        return mark_safe(force_unicode(creole.creole2html(value)))
