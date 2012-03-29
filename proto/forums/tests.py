1ec17dcf043f0d2e95a70afd10e57d5ac3d11e50
Sam Dornan <sdornan@gmail.com>
2012-03-28 17:47:26 -0700

    Good idea to make sure that the post is actually what's being stored

--- proto/forums/tests.py
+++ proto/forums/tests.py
@@ -15,22 +15,22 @@ class ForumModelsTestCase(TestCase):
         thread = Thread.objects.create(forum=self.forum, title='You Have To See This', creator=self.user)
         post = Post.objects.create(thread=thread, body='Look at this cool thing I found.', creator=self.user)
 
-        # It's not cached yet, so it should hit the database
+        # The last post isn't cached yet, so it should hit the database
         with self.assertNumQueries(1):
-            self.forum.last_post()
+            self.assertEqual(self.forum.last_post(), post)
 
         # It's cached now, so it shouldn't hit the database
         with self.assertNumQueries(0):
-            print self.forum.last_post()
+            self.assertEqual(self.forum.last_post(), post)
 
     def test_thread_last_post_caching(self):
         thread = Thread.objects.create(forum=self.forum, title='You Should Check This Out Too', creator=self.user)
         post = Post.objects.create(thread=thread, body="It is way cooler than that other thing.", creator=self.user)
 
-        # The post isn't cached yet, so it should hit the database
+        # The last post isn't cached yet, so it should hit the database
         with self.assertNumQueries(1):
-            thread.last_post()
+            self.assertEqual(thread.last_post(), post)
 
         # It's cached now, so it shouldn't hit the database
         with self.assertNumQueries(0):
-            print thread.last_post()
+            self.assertEqual(thread.last_post(), post)
