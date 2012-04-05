c06f439e6b11c58a558e0de39845b9e3111f6ccc
Sam Dornan <sdornan@gmail.com>
2012-04-05 13:46:47 -0700

    Clean up post editing code, add more docstrings

--- proto/forums/views.py
+++ proto/forums/views.py
@@ -12,12 +12,18 @@ from proto.forums.models import Forum, Thread, Post
 
 
 class ForumListView(ListView):
+    """
+    Displays a list of all the forums.
+    """
     # Retrieve the number of threads and posts in each forum for display with the queryset
     queryset = Forum.on_site.all().annotate(num_threads=Count('thread', distinct=True),
                                             num_posts=Count('thread__post', distinct=True))
 
 
 class ThreadListView(ListView):
+    """
+    Displays a list of the threads in a particular forum.
+    """
     def get_queryset(self):
         self.forum = get_object_or_404(Forum, slug=self.kwargs['forum_slug'], site=settings.SITE_ID)
         return Thread.objects.filter(forum=self.forum).select_related('creator', 'forum').annotate(num_posts=Count('post'))
@@ -29,6 +35,9 @@ class ThreadListView(ListView):
 
 
 class PostListView(ListView):
+    """
+    Displays a list of the posts in a particular thread.
+    """
     def get_queryset(self):
         self.thread = get_object_or_404(Thread.objects.select_related('forum'),
                                         slug=self.kwargs['thread_slug'], forum__site=settings.SITE_ID)
@@ -43,6 +52,9 @@ class PostListView(ListView):
 
 @login_required
 def create_thread(request, forum_slug):
+    """
+    Builds the form for a new thread.
+    """
     forum = get_object_or_404(Forum, slug=forum_slug, site=settings.SITE_ID)
 
     if request.method == 'POST':
@@ -79,27 +91,31 @@ def create_thread(request, forum_slug):
 @login_required
 @require_POST
 def process_post_form(request, forum_slug, thread_slug, post_id=None):
-    if post_id:
-        # User is editing a post
-        post = get_object_or_404(Post, pk=post_id)
-        if post.creator != request.user:
-            raise HttpResponseForbidden()
+    """
+    Processes a form for a post, both create and edit.
+    """
+    created = not post_id
 
     # Makes sure that the user is posting to an existing thread
     thread = get_object_or_404(Thread.objects.select_related('forum'),
                                 slug=thread_slug, forum__slug=forum_slug, forum__site=settings.SITE_ID)
 
-    form = PostForm(request.POST)
+    if not created:
+        # User is editing a post
+        post = get_object_or_404(Post, pk=post_id)
+        if post.creator != request.user:
+            raise HttpResponseForbidden()
+    else:
+        post = Post(creator=request.user, thread=thread)
+
+    form = PostForm(request.POST, instance=post)
     if form.is_valid():
-        # Sets the thread and creator of the post, which aren't part of the form
-        form.instance.thread = thread
-        form.instance.creator = request.user
         form.save()
         # Saves the thread without any changes so that its modified field gets updated
         # and it gets moved to the top of its forum's thread list
         thread.save()
 
-        if post_id:
+        if not created:
             messages.success(request, "Your post was updated successfully.")
         else:
             messages.success(request, "Your post was successful.")
@@ -108,13 +124,16 @@ def process_post_form(request, forum_slug, thread_slug, post_id=None):
 
 
 def ajax_post_form(request, post_id):
+    """
+    Builds a form for a post.
+    """
     if not request.is_ajax():
         raise Http404
 
     post = get_object_or_404(Post.objects, pk=post_id)
-    if post.creator != request.user:
-        raise Http404
+
     post_form = PostForm(instance=post)
+
     return render(request, 'forums/post_form.html', {
         'post_form': post_form,
         'post': post
